6657d0ed2b404810a075b1c2a05b6396caec233c
qa-artem <artem1305@yandex.ru>
2023-02-09 18:01:36 +0300

    added Task0

--- src/main/java/com/simbirSoft/exampleExecution/Task0.java
+++ src/main/java/com/simbirSoft/exampleExecution/Task0.java
@@ -1,4 +1,20 @@
 package com.simbirSoft.exampleExecution;
 
+import java.util.Scanner;
+
 public class Task0 {
+
+    // Реализовать консольный ввод - вывод, выполнение метода здесь
+    public static void main(String[] args) {
+        String inputStr = new Scanner(System.in).nextLine();
+        System.out.println(task0(inputStr));
+    }
+
+    // Писать основную логику здесь
+    protected static Integer task0(String inputStr) {
+        int result = 0;
+        char[] arraySplitInputStr = inputStr.toCharArray();
+        while (result < arraySplitInputStr.length) result++;
+        return result;
+    }
 }
